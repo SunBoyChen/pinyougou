ac28e1554c77c52235c79d3a2024248ee647f2e4
SunBoyChen <32129155+SunBoyChen@users.noreply.github.com>
2018-04-17 23:16:43 +0800

    提交

---
+++ pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Goods.java
@@ -0,0 +1,40 @@
+package com.pinyougou.pojogroup;
+
+import com.pinyougou.pojo.TbGoods;
+import com.pinyougou.pojo.TbGoodsDesc;
+import com.pinyougou.pojo.TbItem;
+
+import java.io.Serializable;
+import java.util.List;
+//创建组合实体类goods
+public class Goods implements Serializable {
+    private TbGoods goods;     //商品spu
+
+    private TbGoodsDesc goodsDesc; //商品扩展
+
+    private List<TbItem> itemList; //商品sku列表
+
+    public TbGoods getGoods() {
+        return goods;
+    }
+
+    public void setGoods(TbGoods goods) {
+        this.goods = goods;
+    }
+
+    public TbGoodsDesc getGoodsDesc() {
+        return goodsDesc;
+    }
+
+    public void setGoodsDesc(TbGoodsDesc goodsDesc) {
+        this.goodsDesc = goodsDesc;
+    }
+
+    public List<TbItem> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<TbItem> itemList) {
+        this.itemList = itemList;
+    }
+}
---
+++ pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Specification.java
@@ -0,0 +1,33 @@
+package com.pinyougou.pojogroup;
+
+import com.pinyougou.pojo.TbSpecification;
+import com.pinyougou.pojo.TbSpecificationOption;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 接受前端传过来的规格
+ */
+public class Specification implements Serializable{
+
+    private TbSpecification tbSpecification;
+
+    private List<TbSpecificationOption> specificationOptionList;
+
+    public TbSpecification getTbSpecification() {
+        return tbSpecification;
+    }
+
+    public void setTbSpecification(TbSpecification tbSpecification) {
+        this.tbSpecification = tbSpecification;
+    }
+
+    public List<TbSpecificationOption> getSpecificationOptionList() {
+        return specificationOptionList;
+    }
+
+    public void setSpecificationOptionList(List<TbSpecificationOption> specificationOptionList) {
+        this.specificationOptionList = specificationOptionList;
+    }
+}
