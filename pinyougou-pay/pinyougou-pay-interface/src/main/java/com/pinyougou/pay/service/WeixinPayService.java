5eab7d5cde6a8c291fddc36800187de0d8aa5a0a
SunBoyChen <32129155+SunBoyChen@users.noreply.github.com>
2018-04-17 23:15:47 +0800

    提交

---
+++ pinyougou-pay/pinyougou-pay-interface/src/main/java/com/pinyougou/pay/service/WeixinPayService.java
@@ -0,0 +1,33 @@
+package com.pinyougou.pay.service;
+
+import java.util.Map;
+
+/**
+ * 微信支付接口
+ * @author Administrator
+ *
+ */
+public interface WeixinPayService {
+
+    /**
+     * 生成微信支付二维码
+     * @param out_trade_no 订单号
+     * @param total_fee 金额(分)
+     * @return
+     */
+    public Map createNative(String out_trade_no, String total_fee);
+
+
+    /**
+     * 查询支付状态
+     * @param out_trade_no
+     */
+    public Map queryPayStatus(String out_trade_no);
+
+    /**
+     * 关闭支付
+     * @param out_trade_no
+     * @return
+     */
+    public Map closePay(String out_trade_no);
+}
